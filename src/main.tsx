4e798fd683aec8140c084785dff69e428942eee8
Aleksa Jovicic <aleksa.j@outlook.com>
2021-10-27 12:03:42 +0200

    Add the movie db API attribution

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "~/App";
+import TmDbAttribution from "~/components/TmDbAttribution";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <TmDbAttribution />
   </React.StrictMode>,
   document.getElementById("root")
 );
